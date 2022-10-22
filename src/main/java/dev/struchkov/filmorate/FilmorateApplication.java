5a568acaedbfdb4aa24de295ebfbdd1151e0282d
Struchkov Mark <mark@struchkov.dev>
2022-10-22 11:04:10 +0300

    InitCommit

---
+++ src/main/java/dev/struchkov/filmorate/FilmorateApplication.java
@@ -0,0 +1,13 @@
+package dev.struchkov.filmorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmorateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmorateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/struchkov/filmorate/FilmorateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.struchkov.filmorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmorateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
